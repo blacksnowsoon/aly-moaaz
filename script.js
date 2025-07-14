0dab6a51a1d3d7dc7275a1b33d0e11281bef7244
gharieb khalifa <blacksnow.soon@gmail.com>
2025-07-14 16:02:17 +0300

    init the project

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+$(document).ready(function() {
+    // --- JavaScript for the Image Slider ---
+    let slideIndex = 1;
+    for (let i = 0; i < 12; i++) {
+        $("#slider-container").append(
+            `<img class="slider-image" src="images/img-${i+1}.png" alt="image-${i+1}">`
+        )
+        $("#slider-dots").append(
+            `<span class="dot" onclick="currentSlide(${i+1})"></span>`
+        )
+    }
+    $("#slider-container").append(
+        `<a class="slider-nav slider-prev" onclick="plusSlides(${-1})">&#10094;</a>
+        <a class="slider-nav slider-next" onclick="plusSlides(${1})">&#10095;</a>`
+    )
+    
+    
+    showSlides(slideIndex);
+    window.plusSlides = function(n) {
+        showSlides(slideIndex += n);
+    }
+    window.currentSlide = function(n) {
+        showSlides(slideIndex = n);
+    }
+   
+    
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("slider-image");
+        let dots = document.getElementsByClassName("dot");
+        
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active-dot", "");
+        }
+    
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active-dot";
+    }
+});
