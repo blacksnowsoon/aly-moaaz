a7a18e29a7990c6bf1d5409cf3bd2859d05d5d33
gharieb khalifa <blacksnow.soon@gmail.com>
2025-07-21 02:57:53 +0300

    uploading the main model

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
     // --- JavaScript for the Image Slider ---
     let slideIndex = 1;
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 8; i++) {
         $("#slider-container").append(
             `<img class="slider-image" src="images/img-${i+1}.png" alt="image-${i+1}">`
         )
